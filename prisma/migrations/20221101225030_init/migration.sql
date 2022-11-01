8f9c5fb5d62d21a513d069ad33052e45b04ffaf4
Guicbdiniz <guicbdeniz@hotmail.com>
2022-11-01 20:04:55 -0300

    feat: create prisma setud files

---
+++ prisma/migrations/20221101225030_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "body" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Admin",
+      posts: {
+        create: [
+          {
+            title: "First Post",
+            body: "This is my first post. I am the Admin",
+          },
+          {
+            title: "Second Post",
+            body: "This is my second post. I am the Admin",
+          },
+        ],
+      },
+    },
+  });
+}
+
+main()
+  .then(() => {
+    console.log("Data seeded...");
+  })
+  .catch((e) => {
+    console.log("Error captured while seeding data", e);
+  });
