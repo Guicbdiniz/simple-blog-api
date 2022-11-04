ac276e7e2f2ea7bdeaf7023837d97804882cbc0b
Guicbdiniz <guicbdeniz@hotmail.com>
2022-11-03 22:01:40 -0300

    doc: update APISchemaBuilder JSDOC

--- src/schema.ts
+++ src/schema.ts
@@ -4,9 +4,21 @@ import { createSchemaBuilder } from "./builder";
 import { addAuthorToSchemaBuilder } from "./models/author";
 import { addPostToSchemaBuilder } from "./models/post";
 
+/**
+ * Wrapper to Pothos schema builder.
+ *
+ * Used to construct APIs with same schema, but different data sources.
+ */
 export class APISchemaBuilder {
   private schema: GraphQLSchema;
 
+  /**
+   * Create new Wrapper.
+   *
+   * @param db PrismaClient where the data will be fetched.
+   *
+   * A mocked data source can be used for testing purposes.
+   */
   constructor(db: PrismaClient) {
     const builder = createSchemaBuilder();
     addAuthorToSchemaBuilder(builder, db);
